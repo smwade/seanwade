2b52e7b348c48501f9cfa1d16b6fd6ef2b0811d0
Sean Wade <seanwademail@gmail.com>
2019-08-25 00:11:16 -0700

    fist commit

---
+++ data/SiteConfig.js
@@ -0,0 +1,32 @@
+const config = {
+  siteTitle: "Sean Wade",
+  siteTitleShort: "seanwade",
+  siteTitleAlt: "Sean Wade",
+  siteLogo: "/logos/logo-1024.png",
+  siteUrl: "https://seanwade.com",
+  pathPrefix: "",
+  siteDescription: "My personal site",
+  siteRss: "/rss.xml",
+  siteFBAppID: "1825356251115265",
+  googleAnalyticsID: "UA-47311644-5",
+  postDefaultCategoryID: "Tech",
+  dateFromFormat: "YYYY-MM-DD",
+  dateFormat: "DD/MM/YYYY",
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/smwade",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Email",
+      url: "mailto:vagr9k@gmail.com",
+      iconClassName: "fa fa-envelope"
+    }
+  ],
+  copyright: "Copyright © 2019. Sean Wade",
+  themeColor: "#c62828",
+  backgroundColor: "#e0e0e0"
+};
+
+module.exports = config;
